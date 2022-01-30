0b7d6ba263442e3141fd64360a8faff416163274
wtubmanbryan <william.tubman@bryanuniversity.edu>
2022-01-30 00:46:46 -0500

    Added sub-folder for selecting-elements and creating index.html and js

---
+++ selecting-elements/js/my-script.js
@@ -0,0 +1,9 @@
+function myFunction(){
+    var h = document.getElementById('myHeader')
+    h.innerHTML="Welcome to my site"
+}
+
+function myOtherFunction(){
+    var h = document.getElementById('myHeader')
+    h.innerHTML="New code sample daily"
+}
