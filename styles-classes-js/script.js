0845bbfd930d4c36825019a195ce84921ee7e39a
wtubmanbryan <william.tubman@bryanuniversity.edu>
2022-02-10 23:47:02 -0500

    Added Creating Changing Styles & Classes Using JS

---
+++ styles-classes-js/script.js
@@ -0,0 +1,9 @@
+for(i = 0; i <5; i++){
+    var myHeader = document.createElement("h2")
+    myHeader.innerHTML = "This is my Assignment"
+    myHeader.style="Font-size: 20px; Font-weight: lighter; Font-family: sans-serif;  Color: cornflowerblue"
+    myHeader.classList="border"
+    document.body.appendChild(myHeader)
+}
+
+
