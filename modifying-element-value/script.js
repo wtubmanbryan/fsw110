4587abc44ae6e611e17f7fcf9b72a856b8ab160b
wtubmanbryan <william.tubman@bryanuniversity.edu>
2022-02-27 00:19:17 -0500

    Added Using Other HTML Inputs

--- modifying-element-value/script.js
+++ modifying-element-value/script.js
@@ -2,13 +2,15 @@ let btn = document.getElementById('submitBtn')
 btn.addEventListener('click',submitForm);
 
 
-function submitForm(){
+function submitForm(event){
+    event.preventDefault();
     let firstName = document.getElementById('firstName')
     let lastName = document.getElementById('lastName')
     let email = document.getElementById('email')
 
-    alert(firstName.value + " " + lastName.value + " " + email.value)
+    alert(firstName.value + "\n" + lastName.value + "\n" + email.value)
     firstName.value =""
     lastName.value=""
     email.value=""
 }
+
--- other-html-inputs/script.js
+++ other-html-inputs/script.js
@@ -1,13 +1,64 @@
 let btn = document.getElementById('submitBtn')
-btn.addEventListener('click',submitBtn)
+btn.addEventListener('click',submitForm)
 
 function submitForm(){
     let firstName = document.getElementById('firstName')
     let lastName = document.getElementById('lastName')
-    let email = document.getElementById('email')
+    let age = document.getElementById('age')
+    let recom = document.getElementById('recom')
+    let glu = document.getElementById('glu')
+    let keto = document.getElementById('keto')
+    let vege = document.getElementById('vege')
 
-    alert(firstName.value + " " + lastName.value + " " + email.value)
-    // firstName.value =""
-    // lastName.value=""
-    // email.value=""
+    let dietary = ""
+    if(glu.checked)
+    {
+        dietary += glu.value
+    }
+    if(keto.checked)
+    {
+        dietary += " "+keto.value
+    }
+    if(vege.checked)
+    {
+        dietary += " "+vege.value
+    }
+
+
+    const rbs = document.querySelectorAll('input[name=shopBefore]');
+            let selectedValue="";
+            for (const rb of rbs) {
+                if (rb.checked) {
+                    selectedValue = rb.value;
+                    break;
+                }
+            }
+
+
+    alert("First Name: "+firstName.value + "\n" + "Last Name: "+lastName.value + "\n" +"Age: "+ age.value+"\n"+ "Shop with us before: " + selectedValue + "\nLikely to recommend us? "+recom.value +"\nDietary Restrictions? " +dietary)
+    firstName.value =""
+    lastName.value=""
+    age.value=""
+    recom.value=""
+
+    for (const rb of rbs) {
+        if (rb.checked) {
+            rb.checked = false
+            break;
+        }
+    }
+
+    if(glu.checked)
+    {
+        glu.checked = false
+    }
+    if(keto.checked)
+    {
+        keto.checked = false
+    }
+    if(vege.checked)
+    {
+        vege.checked = false
+    }
+
 }
