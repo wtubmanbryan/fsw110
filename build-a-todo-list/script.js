b68b6a1c5530c3ee7e73f94302b4034b170f93d6
wtubmanbryan <william.tubman@bryanuniversity.edu>
2022-03-10 01:25:43 -0500

    Added Create A To Do List

---
+++ build-a-todo-list/script.js
@@ -0,0 +1,58 @@
+document.getElementById("save").addEventListener('click', saveToDoList)
+var pid = 1;
+var editId = "";
+
+function saveToDoList() {
+  const mytitle = document.getElementById("title");
+  const mydesc = document.getElementById("desc");
+  if(editId == ""){
+
+    var mydiv = document.createElement("div");
+    mydiv.setAttribute('id', 'post_'+pid) 
+    
+    var title=document.createElement("h4");
+    title.innerText=mytitle.value;
+    var desc=document.createElement("p");
+    desc.innerText=mydesc.value;
+    
+    var btn1 = document.createElement("button");
+    btn1.setAttribute('id', 'btn_'+pid)
+    btn1.setAttribute('class', 'btnSmall')
+    btn1.innerText = "Delete";
+
+    var btnEdit = document.createElement("button");
+    btnEdit.setAttribute('id', 'btnEdit_'+pid)
+    btnEdit.setAttribute('class', 'btnSmall')
+    btnEdit.innerText = "Edit";
+
+      btn1.addEventListener('click', function(e){
+      var mytarget = e.target.parentElement.remove();
+    });
+
+  btnEdit.addEventListener('click', function(e){
+       editId = e.target.parentElement.id
+       mytitle.value = e.target.parentElement.getElementsByTagName("h4")[0].innerText
+       mydesc.value = e.target.parentElement.getElementsByTagName("p")[0].innerText
+       
+    });
+
+    mydiv.appendChild(title);
+    mydiv.appendChild(desc);
+    mydiv.appendChild(btn1);
+    mydiv.appendChild(btnEdit);
+    
+    const myobj = document.getElementById("myList");
+    myList.appendChild(mydiv);
+    
+    pid +=1;
+  }
+  else{
+    
+    document.getElementById(editId).getElementsByTagName("h4")[0].innerText = document.getElementById("title").value;
+    document.getElementById(editId).getElementsByTagName("p")[0].innerText = document.getElementById("desc").value;
+    editId = "";
+  }
+  mytitle.value="";
+  mydesc.value ="";
+  
+}
